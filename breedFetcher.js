3c596907dbe958f6cdc7457b9a080086212f84dc
Ruanruanx <zxin7@uwo.ca>
2022-08-10 20:35:00 +0000

    edge case

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
-const  request = require('request');
+const request = require('request');
 const parameter = process.argv[2];
-request('https://api.thecatapi.com/v1/breeds/search?q='+parameter, (error, response, body) => {
+request('https://api.thecatapi.com/v1/breeds/search?q=' + parameter, (error, response, body) => {
   if (error) {
     console.log('error:', error);
   }
   if (response) {
     console.log('statusCode:', response.statusCode);
   }
-  if (body) {
+  if (body === '[]') {
+    console.log('No results found');
+  }
+  else if (body) {
     const data = JSON.parse(body);
-    console.log(data[0].description);
+    //console.log(data[0].description);
     //console.log(typeof(data));
   }
 });
